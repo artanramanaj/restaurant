7fe3d0041585b97f7f918b85a0da0b75d3a30073
Artan Rramanaj <artan.ramanaj@finnie.consulting>
2026-02-26 16:25:26 +0100

    configs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { HomeScreen, AboutScreen } from "@/screens/";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/about" element={<AboutScreen />} />
+    </Route>,
+  ),
+);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <ToastContainer />
   </StrictMode>,
-)
+);
---
+++ frontend/src/screens/AboutScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutScreen = () => {
+  return (
+    <div className="container">
+      <h2>About</h2>
+    </div>
+  );
+};
+
+export default AboutScreen;

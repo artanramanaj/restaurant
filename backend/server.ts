a6ad47719fe4dc3d2acf18fb77add8e5c86e833c
Artan <artan.rramanaj@gmail.com>
2026-03-05 14:33:16 +0100

    db connection

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
-console.log(process.env.PORT);
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Connection failed:", err));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from TypeScript + Express!");
 });
